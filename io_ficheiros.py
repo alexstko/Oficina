fc23fa7048376c7a599e65964e4c5c5a8c650fdb
alex <a79434@ualg.pt>
2023-12-03 02:06:14 +0000

    Modifica o ficheiro io ficheiros

--- io_ficheiros.py
+++ io_ficheiros.py
@@ -54,10 +54,4 @@ def le_de_ficheiro(nome_ficheiro):
         return pickle.load(f)
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
-# todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
 
-# ...
