c847d000564bff5b13f2ffda1561802b79360d2e
alex <a79434@ualg.pt>
2023-12-03 02:05:27 +0000

    Modifica o ficheiro clientes

--- clientes.py
+++ clientes.py
@@ -21,11 +21,3 @@ def cria_novo_cliente():
 
     pass
 
-
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome clientes-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro clientes.py existente, deve apagar
-# todos os outros ficheiros clientes-*.py, e inclusive estes comentários
-
-# ...
