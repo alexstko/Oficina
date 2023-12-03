88129ad7287b2e7a8fe80d6ee6d9bdf3b64a6b3b
alex <a79434@ualg.pt>
2023-12-03 02:05:47 +0000

    Modifica o ficheiro faturas

--- faturas.py
+++ faturas.py
@@ -28,10 +28,3 @@ def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
     # TODO: Implementar esta função
     # ...
     pass
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome faturas*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro faturas.py existente, deve apagar
-# todos os outros ficheiros faturas-*.py, e inclusive estes comentários
-
-# ...
